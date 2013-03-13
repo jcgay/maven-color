ae2fe10d03f571d2599c9fef227fe34cec95b641
Jean-Christophe GAY <contact@jeanchristophegay.com>
2013-03-13 16:01:54 +0100

    Create a Logger implementation using Jansi.

---
+++ src/main/java/com/github/jcgay/maven/color/logger/AnsiColorLogger.java
@@ -0,0 +1,87 @@
+package com.github.jcgay.maven.color.logger;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.maven.Maven;
+import org.codehaus.plexus.logging.AbstractLogger;
+import org.codehaus.plexus.logging.Logger;
+import org.fusesource.jansi.AnsiConsole;
+
+import java.io.PrintStream;
+
+import static org.fusesource.jansi.Ansi.Color.RED;
+import static org.fusesource.jansi.Ansi.Color.YELLOW;
+import static org.fusesource.jansi.Ansi.ansi;
+
+/**
+ * User: jcgay
+ */
+public class AnsiColorLogger extends AbstractLogger {
+
+    private PrintStream out;
+
+    private static final String WARNING = "[WARNING] ";
+    private static final String ERROR = "[ERROR] ";
+    private static final String INFO = "[INFO] ";
+    private static final String DEBUG = "[DEBUG] ";
+    private static final String FATAL = "[FATAL] ";
+
+    public AnsiColorLogger() {
+        super(LEVEL_INFO, Maven.class.getName());
+        out = AnsiConsole.out;
+    }
+
+    @VisibleForTesting AnsiColorLogger(PrintStream out) {
+        super(LEVEL_DEBUG, Maven.class.getName());
+        if (out == null) {
+            throw new IllegalArgumentException("A PrintStream is mandatory.");
+        }
+        this.out = out;
+    }
+
+    public void debug(String message, Throwable throwable) {
+        if (isDebugEnabled()) {
+            out.print(DEBUG);
+            out.println(message);
+            printStackTrace(throwable);
+        }
+    }
+
+    public void info(String message, Throwable throwable) {
+        if (isInfoEnabled()) {
+            out.print(INFO);
+            out.println(message);
+            printStackTrace(throwable);
+        }
+    }
+
+    public void warn(String message, Throwable throwable) {
+        if (isWarnEnabled()) {
+            out.println(ansi().fgBright(YELLOW).a(WARNING).a(message).reset());
+            printStackTrace(throwable);
+        }
+    }
+
+    public void error(String message, Throwable throwable) {
+        if (isErrorEnabled()) {
+            out.println(ansi().fgBright(RED).bold().a(ERROR).a(message).reset());
+            printStackTrace(throwable);
+        }
+    }
+
+    public void fatalError(String message, Throwable throwable) {
+        if (isFatalErrorEnabled()) {
+            out.println(ansi().fgBright(RED).bold().a(FATAL).a(message).reset());
+            printStackTrace(throwable);
+        }
+    }
+
+    public Logger getChildLogger(String name) {
+        return this;
+    }
+
+    private void printStackTrace(Throwable throwable) {
+        if (throwable != null) {
+            throwable.printStackTrace(out);
+        }
+    }
+}
---
+++ src/test/java/com/github/jcgay/maven/color/logger/AnsiColorLoggerTest.java
@@ -0,0 +1,152 @@
+package com.github.jcgay.maven.color.logger;
+
+import org.codehaus.plexus.logging.Logger;
+import org.fest.assertions.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * User: jcgay
+ */
+public class AnsiColorLoggerTest {
+
+    private AnsiColorLogger logger;
+    private ByteArrayOutputStream result;
+
+    @Before
+    public void init() {
+        result = new ByteArrayOutputStream();
+        logger = new AnsiColorLogger(new PrintStream(result));
+    }
+
+    @Test
+    public void should_log_a_warning_message_in_yellow() throws Exception {
+
+        logger.warn("text in yellow");
+
+        assertThat(result.toString()).isEqualTo("\u001B[93m[WARNING] text in yellow\u001B[m\n");
+    }
+
+    @Test
+    public void should_log_an_exception_with_level_warning() throws Exception {
+
+        logger.warn("message", new NullPointerException("exception"));
+
+        assertThat(result.toString()).contains("exception");
+    }
+
+    @Test
+    public void should_not_log_if_level_warn_is_not_enabled() throws Exception {
+
+        logger.setThreshold(Logger.LEVEL_DISABLED);
+        logger.warn("message");
+
+        assertThat(result.toString()).doesNotContain("message");
+    }
+
+    @Test
+    public void should_log_an_error_message_in_red() throws Exception {
+
+        logger.error("text in red");
+
+        assertThat(result.toString()).isEqualTo("\u001B[91;1m[ERROR] text in red\u001B[m\n");
+    }
+
+    @Test
+    public void should_log_an_exception_with_level_error() throws Exception {
+
+        logger.error("message", new NullPointerException("exception"));
+
+        assertThat(result.toString()).contains("exception");
+    }
+
+    @Test
+    public void should_not_log_if_level_error_is_not_enabled() throws Exception {
+
+        logger.setThreshold(Logger.LEVEL_DISABLED);
+        logger.error("message");
+
+        assertThat(result.toString()).doesNotContain("message");
+    }
+
+    @Test
+    public void should_not_modify_message_when_logging_in_debug() throws Exception {
+
+        logger.debug("message");
+
+        assertThat(result.toString()).isEqualTo("[DEBUG] message\n");
+    }
+
+    @Test
+    public void should_log_an_exception_with_level_debug() throws Exception {
+
+        logger.debug("message", new NullPointerException("exception"));
+
+        assertThat(result.toString()).contains("exception");
+    }
+
+    @Test
+    public void should_not_log_if_level_debug_is_not_enabled() throws Exception {
+
+        logger.setThreshold(Logger.LEVEL_DISABLED);
+        logger.debug("message");
+
+        assertThat(result.toString()).doesNotContain("message");
+    }
+
+    @Test
+    public void should_not_modify_message_when_logging_in_info() throws Exception {
+
+        logger.info("message");
+
+        assertThat(result.toString()).isEqualTo("[INFO] message\n");
+    }
+
+    @Test
+    public void should_log_an_exception_with_level_info() throws Exception {
+
+        logger.info("message", new NullPointerException("exception"));
+
+        assertThat(result.toString()).contains("exception");
+    }
+
+    @Test
+    public void should_not_log_if_level_info_is_not_enabled() throws Exception {
+
+        logger.setThreshold(Logger.LEVEL_DISABLED);
+        logger.info("message");
+
+        assertThat(result.toString()).doesNotContain("message");
+    }
+
+    @Test
+    public void should_log_a_fatal_message_in_red() throws Exception {
+
+        logger.fatalError("text in red");
+
+        assertThat(result.toString()).isEqualTo("\u001B[91;1m[FATAL] text in red\u001B[m\n");
+    }
+
+    @Test
+    public void should_log_an_exception_with_level_fatal() throws Exception {
+
+        logger.fatalError("message", new NullPointerException("exception"));
+
+        assertThat(result.toString()).contains("exception");
+    }
+
+    @Test
+    public void should_not_log_if_level_fatal_is_not_enabled() throws Exception {
+
+        logger.setThreshold(Logger.LEVEL_DISABLED);
+        logger.fatalError("message");
+
+        assertThat(result.toString()).doesNotContain("message");
+    }
+}
